2d185495ae83831d3b1749c66f58f1daa874a4b6
avvet <sveta.drozdovich@yandex.ru>
2018-04-02 14:15:55 +0300

    fisrt axios promise added

---
+++ src/js/http/http-service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = 'http://localhost:3000/';
+const USERS = 'users';
+const COMP = 'companies';
+const DIVIDER = '/';
+
+
+let funct = function getAll(){
+  axios.get(BASE_URL + USERS + DIVIDER + '1')
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+// let httpService = new HttpServiceClass();
+// export {httpService};
+export {funct};
--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,15 @@ import $ from 'jquery';
 import {func} from './second';
 func();
 
+// import {httpService} from "./http/http-service";
+import {funct} from "./http/http-service";
+
 console.log('Hello,world');
+
+let getPostsBtn = document.getElementById('btn');
+let singlePostBtn = document.getElementById('single_btn');
+let myParagraph = document.getElementById('paragraph');
+
+getPostsBtn.onclick = () => {
+  funct();
+};
